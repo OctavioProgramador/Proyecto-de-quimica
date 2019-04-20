1064061613ca32a15560b609c71e5173ce12b511
Aaron Velazquez <47768260+GuiltyGG@users.noreply.github.com>
2019-04-19 17:54:35 -0700

    XD

--- Consola/Questionary/ScoreTable.cs
+++ Consola/Questionary/ScoreTable.cs
@@ -1,31 +1,66 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+using System.IO;
+
 
 namespace Questionary
 {
+    [Serializable()]
     class ScoreTable
     {
+        public string rute;
         private List<Player> record;
         public ScoreTable()
         {
+            rute = Directory.GetCurrentDirectory() + @"\playerScore.xml";
             record = new List<Player>();    
         }
+
         public void Print()
         {
-            foreach(Player player in record)
+     
+            foreach (Player player in record)
             {
-                Console.WriteLine(player.ToString());
+              Console.WriteLine(player.ToString());
             }
+            Serialize();
         }
+
         public void AddPuntuation(Player newPuntuation)
         {
+                
             record.Add(newPuntuation);
             record.Sort();
-            if(record.Count>15)
+      
+            if (record.Count > 15)
             {
-                record.Remove(record[15]);
+                    record.Remove(record[15]);
+            }
+                
+        }
+
+        public void Deserialize()
+        {
+            using (FileStream fs = File.OpenRead(rute))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Player>));
+                record = (List<Player>)serializer.Deserialize(fs);
             }
         }
+
+        public void Serialize()
+        {
+            using (TextWriter fs = new StreamWriter(rute))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Player>));
+                serializer.Serialize(fs, record);
+            }
+        }
+
     }
 }
